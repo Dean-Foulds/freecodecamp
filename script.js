4bb0162aaa194947c99ed87ebf05e6669a7ae8a1
Dean-Foulds <deanfoulds@gmail.com>
2016-11-05 18:23:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-$("#search").on("click", function() {
+
 $("#searchCriteria").keypress(function(e) {
   if (e.which == 13) {
     $('#search').trigger('click');
   }
-  });
   var searchCriteria = $("#searchCriteria").val();
   console.log(searchCriteria);
   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchCriteria + "&format=json&callback=?";
@@ -20,5 +19,26 @@ $("#searchCriteria").keypress(function(e) {
       }
     }
   })
-})
+});
+
+
+$("#search").on("click", function() {
+
+var searchCriteria = $("#searchCriteria").val();
+  console.log(searchCriteria);
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchCriteria + "&format=json&callback=?";
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    async: true,
+    success: function(data, status, jqXHR) {
+      $("#output").html();
+      for (var i = 0; i < data[1].length; i++) {
+        $("#output").prepend("<div><div class='well'><a href=" + data[3][i] + "><h2 class='font-color'>" + data[1][i] + "</h2>" + "<p class='font-color'>" + data[2][i] + "</p></a></div></div>");
+      }
+    }
+  })
+});
 
